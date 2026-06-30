3f0afe7e318b19b84a91c64ab9acd585144a82e9
w1zzyakik <ohrickaaivan@gmail.com>
2026-06-30 15:43:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ const btn = document.getElementById('mainButton');
 const sound = document.getElementById('prankSound');
 
 btn.addEventListener('click', () => {
-    // Запускаем звук
+    // Просто запускаем звук
     sound.play();
-
-    // Визуальный эффект при нажатии (исчезновение)
+    
+    // Анимация кнопки (исчезновение)
     btn.style.transition = "all 0.5s";
     btn.style.opacity = "0";
     btn.style.transform = "scale(2)";
-
-    // Переход через 0.5 сек
-    setTimeout(() => {
-        window.location.href = "https://youtube.com/watch?v=dQw4w9WgXcQ";
-    }, 500);
+    
+    // Убрали window.location.href, теперь редиректа не будет
 });
